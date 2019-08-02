3e23a904a1af085285775edf471649b642d22972
ligazi <1442870186@qq.com>
2019-08-02 12:15:44 +0800

    创建其他项目

---
+++ guilimall-pms/src/test/java/com/atguigu/guilimall/pms/GuilimallPmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.guilimall.pms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuilimallPmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
